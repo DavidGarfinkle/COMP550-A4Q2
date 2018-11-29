3ce4d31114410bc998a27d49e002ad7ef5fd5ebe
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-11-28 22:31:25 -0500

    New: results generator script

---
+++ generate-results.sh
@@ -0,0 +1,1 @@
+for i in orig best-avg simplified leading; do for a in 1-1 1-2 1-3 1-4 2-1 2-2 2-3 2-4 3-1 3-2 3-3 3-4 4-1 4-2 4-3 4-4; do python sumbasic.py leading docs/doc4-4.txt > results/docs4-4-leading.txt; done; done;
