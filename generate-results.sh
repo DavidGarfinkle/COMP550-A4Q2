bea8f5d087b7b02473cce2ff566b088aa65171bb
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-11-28 22:36:37 -0500

    Fix: parameterize generation script

--- generate-results.sh
+++ generate-results.sh
@@ -1,1 +1,1 @@
-for i in orig best-avg simplified leading; do for a in 1-1 1-2 1-3 1-4 2-1 2-2 2-3 2-4 3-1 3-2 3-3 3-4 4-1 4-2 4-3 4-4; do python sumbasic.py leading docs/doc4-4.txt > results/docs4-4-leading.txt; done; done;
+for i in orig best-avg simplified leading; do for a in 1-1 1-2 1-3 1-4 2-1 2-2 2-3 2-4 3-1 3-2 3-3 3-4 4-1 4-2 4-3 4-4; do python sumbasic.py $i docs/doc$a.txt > results/docs$a-$i.txt; done; done;
